f66c267d272814aa9e446e967a4b37bd6ccde6bd
Damego <damego.dev@gmail.com>
2025-05-20 21:16:54 +0500

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Bully
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            connectBtn = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            nodeTB = new System.Windows.Forms.RichTextBox();
+            statusTB = new System.Windows.Forms.RichTextBox();
+            SuspendLayout();
+            // 
+            // connectBtn
+            // 
+            connectBtn.Location = new System.Drawing.Point(19, 78);
+            connectBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            connectBtn.Name = "connectBtn";
+            connectBtn.Size = new System.Drawing.Size(106, 36);
+            connectBtn.TabIndex = 5;
+            connectBtn.Text = "Подключиться";
+            connectBtn.UseVisualStyleBackColor = true;
+            connectBtn.Click += connectBtn_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
+            label1.Location = new System.Drawing.Point(19, 9);
+            label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(97, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Номер узла";
+            // 
+            // nodeTB
+            // 
+            nodeTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
+            nodeTB.Location = new System.Drawing.Point(19, 37);
+            nodeTB.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            nodeTB.Name = "nodeTB";
+            nodeTB.Size = new System.Drawing.Size(106, 35);
+            nodeTB.TabIndex = 3;
+            nodeTB.Text = "";
+            // 
+            // statusTB
+            // 
+            statusTB.Enabled = false;
+            statusTB.Location = new System.Drawing.Point(133, 5);
+            statusTB.Name = "statusTB";
+            statusTB.Size = new System.Drawing.Size(261, 140);
+            statusTB.TabIndex = 6;
+            statusTB.Text = "";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(397, 144);
+            Controls.Add(statusTB);
+            Controls.Add(connectBtn);
+            Controls.Add(label1);
+            Controls.Add(nodeTB);
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Name = "MainForm";
+            Text = "Main";
+            FormClosing += MainForm_FormClosing;
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox nodeTB;
+        private System.Windows.Forms.RichTextBox statusTB;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YamlDotNet.Serialization;
+
+namespace Bully
+{
+    public partial class MainForm : Form
+    {
+        private int? nodeId; // Текущий номер узла
+        private int? port; // Текущий порт узла
+        private List<Dictionary<string, int>> allNodes; // Список всех зарегистрированных узлов
+        private TcpListener listener; // Слушатель подключений к узлу
+        private Thread listenerThread; // Поток для слушателя
+        private List<Node> connectedNodes = new(); // Список подключенных узлов. В конечном итоге должен совпадать со список зарегистрированных узлов
+        private bool _continue = true; // Флаг на продолжение работы программы
+        private int highestNodeId; // Самый старший возможный узел
+        private Node coordinatorNode; // Узел, который является координатором
+        private Task coordinatorPingTask; // Таска на пинг координатора
+        private bool ponged; // Флаг, означающий, был ли принято сообщение PONG от координатора
+        private bool receivedOk; // Флаг, означающий, был ли принято сообщение OK хотя бы от одного узла
+
+        public MainForm()
+        {
+            InitializeComponent();
+            allNodes = ReadNodes();
+
+            var highestNode = allNodes.OrderBy(node => node["id"]).Last();
+            highestNodeId = highestNode["id"];
+        }
+
+        private List<Dictionary<string, int>> ReadNodes()
+        {
+            string nodesInfo = File.ReadAllText("./nodes.yaml");
+            var deserializer = new DeserializerBuilder().Build();
+            return deserializer.Deserialize<List<Dictionary<string, int>>>(nodesInfo);
+        }
+
+        private int GetNodeIdByPort(int port)
+        {
+            return allNodes.Find(node => node["port"] == port)["id"];
+        }
+
+        private int GetPortByNodeId(int nodeId)
+        {
+            return allNodes.Find(node => node["id"] == nodeId)["port"];
+        }
+
+        private IPAddress GetIPAddress()
+        {
+            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress IP = hostEntry.AddressList[0];
+            foreach (IPAddress address in hostEntry.AddressList)
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    IP = address;
+                    break;
+                }
+            return IP;
+        }
+
+        private async Task ReceiveConnections()
+        {
+            while (_continue)
+            {
+                TcpClient client = null;
+                try
+                {
+                    client = listener.AcceptTcpClient();
+                    // Приняли подключение и ожидаем сообщения HELLO с номером узла
+                }
+                catch (Exception ex)
+                {
+                    if (!_continue)
+                        return;
+                    continue;
+                }
+            }
+        }
+
+        private async Task ReceiveMessages(Socket socket)
+        {
+            while (_continue)
+            {
+                byte[] buff = new byte[1024];
+                await socket.ReceiveAsync(buff);
+
+                string[] rawMessage = Encoding.Unicode.GetString(buff).Split(";");
+                string message = rawMessage[0];
+                int nodeId = int.Parse(rawMessage[1]);
+
+                await ProcessMessage(message, nodeId);
+            }
+        }
+
+        private async Task ProcessMessage(string message, int senderNodeId)
+        {
+            var senderNode = connectedNodes.Find(node => node.Id == senderNodeId);
+
+            if (message == "ELECTION")
+            {
+                AddStatusText("Получено ELECTION", senderNodeId);
+                // Младшие узлы отправляют старшим сообщение ELECTION
+                // Если старший узел живой, то он отправляет ELECTION другим, более старшим узлам
+                await SendMessage(senderNode.Client, "OK");
+                await SendElectionMessage();
+            }
+            else if (message == "OK")
+            {
+                AddStatusText("Получено OK", senderNodeId);
+                receivedOk = true;
+            }
+            else if (message == "COORDINATOR")
+            {
+                AddStatusText("Получено COORDINATOR", senderNodeId);
+                coordinatorNode = senderNode;
+                coordinatorPingTask = Task.Factory.StartNew(PingCoordinator);
+            }
+            else if (message == "PING")
+            {
+                AddStatusText("Получено PING", senderNodeId);
+                await SendMessage(senderNode.Client, "PONG");
+            }
+            else if (message == "PONG")
+            {
+                AddStatusText("Получено PONG", senderNodeId);
+                ponged = true;
+            }
+            else if (message == "HELLO")
+            {
+
+            }
+        }
+
+        /*
+        Отправляет всем старшим узлам сообщение ELECTION.
+        */
+        private async Task SendElectionMessage()
+        {
+            coordinatorNode = null;
+            receivedOk = false;
+
+            foreach (var node in connectedNodes)
+            {
+                if (node.Id > nodeId) continue;
+
+                await SendMessage(node.Client, "ELECTION");
+            }
+
+            // Ожидаем в течение двух секунд ответа ОК хотя бы от одного узла. Если ответ не пришёл, то объявляем себя координатором
+            await Task.Delay(2000);
+            if (!receivedOk)
+            {
+                receivedOk = false;
+                await SendCoordinatorMessage();
+            }
+        }
+
+        private async Task SendCoordinatorMessage()
+        {
+            foreach (var node in connectedNodes)
+            {
+                await SendMessage(node.Client, "COORDINATOR");
+            }
+        }
+
+        private async Task SendMessage(TcpClient client, string message)
+        {
+            byte[] buff = Encoding.Unicode.GetBytes(message);
+            await client.Client.SendAsync(buff);
+        }
+
+        /*
+        Для того, чтобы понять, что координатор вообще живой, текущий узел будет с некоторой периодичностью отправлять ему сообщение.
+        Если ответ не поступит, то узел начнёт ELECTION
+        */
+        private async Task PingCoordinator()
+        {
+            while (true)
+            {
+                ponged = false;
+                await SendMessage(coordinatorNode.Client, "PING");
+
+                await Task.Delay(2 * 1000);
+                if (!ponged)
+                {
+                    await SendElectionMessage();
+                    return;
+                }
+            }
+        }
+
+        private void ConnectToAllNodes()
+        {
+            foreach (var node in allNodes.Where(node => node["id"] != nodeId))
+            {
+                int id = node["id"];
+                try
+                {
+                    AddStatusText($"Попытка подключиться к {id}");
+                    ConnectToNode(node["id"], node["port"]);
+                }
+                catch (Exception ex)
+                {
+                    AddStatusText($"Не удалось подключиться к {id}");
+                    Debug.WriteLine(ex.Message);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        private async Task ConnectToNode(int connectNodeId, int connectPort)
+        {
+            var ip = GetIPAddress();
+            
+            var client = new TcpClient(ip.ToString(), connectPort);
+            await SendMessage(client, $"HELLO;{(int)nodeId}");
+        }
+
+        private void AddNode(int nodeId, TcpClient client)
+        {
+            var receiverTask = Task.Factory.StartNew(() => ReceiveMessages(client.Client, nodeId));
+            var node = new Node(nodeId, client, receiverTask);
+            connectedNodes.Add(node);
+        }
+
+        private void AddStatusText(string text)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                statusTB.Text = $"{text}\n{statusTB.Text}";
+            }));
+        }
+
+        private void AddStatusText(string text, int nodeId)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                statusTB.Text = $"{nodeId}: {text}\n{statusTB.Text}";
+            }));
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            if (nodeId != null)
+            {
+                Disconnect();
+
+                this.Invoke(new MethodInvoker(() => {
+                    connectBtn.Text = "Подключиться";
+                    nodeTB.Enabled = true;
+                }));
+
+                return;
+            }
+
+            nodeId = int.Parse(nodeTB.Text);
+            port = GetPortByNodeId((int)nodeId);
+
+            try
+            {
+                listener = new TcpListener(GetIPAddress(), (int)port);
+                listener.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            listenerThread = new Thread(ReceiveConnections);
+            listenerThread.Start();
+
+            ConnectToAllNodes();
+
+            // Текущий узел является самым старшим среди всех остальных узлов, поэтому с чистой совестью он объявляет себя координатором
+            if (nodeId == highestNodeId)
+                SendCoordinatorMessage();
+            else
+                SendElectionMessage();
+
+            this.Invoke(new MethodInvoker(() => {
+                connectBtn.Text = "Отключиться";
+                nodeTB.Enabled = false;
+            }));
+        }
+
+        private void Disconnect()
+        {
+            nodeId = null;
+            port = null;
+            _continue = false;
+
+            foreach (var node in connectedNodes)
+            {
+                node.ReceiverTask.Dispose();
+                node.Client.Close();
+            }
+            if (coordinatorPingTask != null)
+            {
+                coordinatorPingTask.Dispose();
+            }
+
+            if (listener != null)
+                listener.Stop();
+            if (listenerThread != null)
+                listenerThread.Interrupt();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Disconnect();
+        }
+    }
+
+    class Node
+    {
+        public int Id { get; set; }
+        public TcpClient Client { get; set; }
+        public Task ReceiverTask { get; set; }
+
+        public Node(int id, TcpClient client, Task receiverTask)
+        {
+            Id = id;
+            Client = client;
+            ReceiverTask = receiverTask;
+        }
+    }
+}
